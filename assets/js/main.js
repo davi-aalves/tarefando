e06f9f5d78bcdd4d629495f251337ade156fe552
Davi Alves <daviih2000@gmail.com>
2025-01-23 17:55:09 -0300

    feat: adicionar funcionalidade para salvar tarefas no localStorage
    - Criada a função `salvarTarefas` para salvar as tarefas no localStorage.
    - A função transforma as tarefas em um array de strings, removendo o texto do botão "Apagar".
    - As tarefas são convertidas para JSON e armazenadas no localStorage para persistência.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,6 @@ const inputNovaTarefa = document.querySelector(".input-nova-tarefa");
 const btnAddTarefa = document.querySelector(".btn-add-tarefa");
 const tarefas = document.querySelector(".tarefas");
 
-btnAddTarefa.addEventListener("click", function () {
-  if (!inputNovaTarefa.value) return;
-  criaTarefa(inputNovaTarefa.value);
-});
-
 function criaLi() {
   const li = document.createElement("li");
   return li;
@@ -25,6 +20,7 @@ function criaBotaoApagar(li) {
   const botaoApagar = document.createElement("button");
   botaoApagar.innerText = "Apagar";
   botaoApagar.setAttribute("class", "apagar");
+  botaoApagar.setAttribute("title", "Apagar esta tarefa");
   li.appendChild(botaoApagar);
 }
 
@@ -33,11 +29,33 @@ function criaTarefa(textoInput) {
   li.innerHTML = textoInput;
   tarefas.appendChild(li);
   criaBotaoApagar(li);
+  salvarTarefas();
 }
+
+btnAddTarefa.addEventListener("click", function () {
+  if (!inputNovaTarefa.value) return;
+  criaTarefa(inputNovaTarefa.value);
+});
 
 document.addEventListener("click", function (e) {
   const el = e.target;
+
   if (el.classList.contains("apagar")) {
     el.parentElement.remove();
+    salvarTarefas();
   }
 });
+
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll("li");
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace("Apagar", "").trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+}
+
+const tarefasJSON = JSON.stringify(listaDeTarefas);
+localStorage.setItem("tarefas", tarefasJSON);
